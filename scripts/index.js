713da465d77ec09d274337d8e0469fba5cb77ec9
IsaacBoateng <ikenana454@gmail.com>
2024-07-04 14:20:26 -0400

    Project4 Stage 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,32 @@
 const initialCards=[
 
-{Title : 'Yosemite Valley',
+{
+    Title : 'Yosemite Valley',
     link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg'
 },
 
-{Title : 'Lake Louise',
+{
+    Title : 'Lake Louise',
     link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg'
 },
 
-{Title : 'Bald Mountains',
+{
+    Title : 'Bald Mountains',
     link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg'
 },
 
-{Title : 'Latemar',
+{
+    Title : 'Latemar',
     link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg'
 },
 
-{Title : 'Vanoise National Park',
+{
+    Title : 'Vanoise National Park',
     link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg'
 },
 
-{Title : 'YLago di Braies',
+{
+    Title : 'YLago di Braies',
     link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg'
 }
 
