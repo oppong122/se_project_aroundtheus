b55ecf30d45ec73e4862735049120022204fe8b2
IsaacBoateng <ikenana454@gmail.com>
2024-07-09 09:11:48 -0400

    Project 4 Stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,43 @@
 const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
 
-{
-    name : 'Yosemite Valley',
-    link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg'
-},
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
 
-{
-    name : 'Lake Louise',
-    link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg'
-},
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
 
-{
-    name : 'Bald Mountains',
-    link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg'
-},
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
 
-{
-    name : 'Latemar',
-    link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg'
-},
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
 
-{
-    name : 'Vanoise National Park',
-    link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg'
-},
-
-{
-    name : 'YLago di Braies',
-    link : 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg'
-}
-
+  {
+    name: "YLago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
 
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_open");
+});
+
+const profileCloseButton = document.querySelector("#profile-close-button");
+profileCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_open");
+});
